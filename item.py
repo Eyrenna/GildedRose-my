af1a5358566dd82fa5ad12dd4db8f39165bce181
Claudia Gázquez Alessio <claudia365x@gmail.com>
2019-01-08 18:40:18 +0100

    Clase Updatable añadido

--- item.py
+++ item.py
@@ -1,21 +1,34 @@
-class Item():
+from updatable import Updatable
+
+
+class Item(Updatable):
+
+
     def __init__(self, name, sell_in, quality):
         self.name = name
         self.sell_in = sell_in
         self.quality = quality
+
     
     def getname(self): 
         return self.name
+
     
     def getsell_in(self):
         return self.sell_in
+
 
     def getquality(self):
         return self.quality
 
 
 if __name__ == "__main__":
+
+    # CASOS TEST __init__
     pato = Item("pato", "100", "50")
     assert pato.getname() == "pato"
     assert pato.getsell_in() == "100"
     assert pato.getquality() == "50"
+
+    # CASOS TEST Udatable
+    assert pato.update_quality() == None
