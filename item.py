ed72325b9ba88f33b75b2ce775276e0b7189214a
Claudia Gázquez Alessio <claudia365x@gmail.com>
2019-01-09 19:23:22 +0100

    Dejarlo como dijo Dobby, quitando los getS

--- item.py
+++ item.py
@@ -6,29 +6,13 @@ class Item():
         self.sell_in = sell_in
         self.quality = quality
 
-    
-    def getname(self): 
-        return self.name
-
-    
-    def getsell_in(self):
-        return self.sell_in
-
-
-    def getquality(self):
-        return self.quality
 
     def __repr__(self):
         return '%s, %s, %s' % (self.name, self.sell_in, self.quality)
 
-
+    
 if __name__ == "__main__":
 
-    # CASOS TEST __init__
-    pato = Item("pato", "100", "50")
-    assert pato.getname() == "pato"
-    assert pato.getsell_in() == "100"
-    assert pato.getquality() == "50"
-
-    # CASOS TEST __repr__
+    # "CASOS TEST" __repr__
+    pato = Item("pato", 100, 50)
     print(pato)
